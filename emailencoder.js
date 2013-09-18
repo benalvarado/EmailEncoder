3fee54b699c1068e055e9caedb1bca4cc35c59df
Ben Alvarado <ben@12limes.com>
2013-09-17 23:22:39 -0500

    cleaned up code output from EmailEncoder.encode

--- emailencoder.js
+++ emailencoder.js
@@ -1,12 +1,12 @@
 var EmailEncoder = EmailEncoder || {};
 
 EmailEncoder.encode = function (email) {
-    var estr = '<a href=mailto:' + email + '>' + email + '</a>';
+    var estr = "<a href=mailto:" + email + ">" + email + "</a>";
     var strlength = estr.length;
     var intcodes = [];
 
-    var start_code = '<script>c=';
-    var end_code = '.split(",");l=c.length;e="";for(i=0;i<l;i++){e+=String.fromCharCode(c[i]);}document.write(e);</script>';
+    var start_code = "<script>c=";
+    var end_code = ".split(',');l=c.length;e='';for(i=0;i<l;i++){e+=String.fromCharCode(c[i]);}document.write(e);</script>";
     var code = "";
 
     for(var i = 0; i < strlength; i++) {
